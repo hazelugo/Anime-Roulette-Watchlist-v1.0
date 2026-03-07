a3c0b05f3e319e273a4859d0b878a0f0f5a87e16
Hector Lugo <haze.lugo@gmail.com>
2026-03-07 15:23:19 -0600

    Added watchlist layout

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -2,6 +2,7 @@
 import { computed } from 'vue'
 import AnimeCard from '@/components/AnimeCard.vue'
 import { useAnimeRoulette } from '@/composables/useAnimeRoulette'
+import Watchlist from '@/components/Watchlist.vue'
 
 const { anime, loading, error, spin, cooldownLeft } = useAnimeRoulette()
 
@@ -68,6 +69,7 @@ const spinLabel = computed(() => {
             :anime="anime"
           />
         </section>
+        <Watchlist />
       </div>
     </div>
   </main>
